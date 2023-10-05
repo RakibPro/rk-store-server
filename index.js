f7b8c805935bf94dd86aaaface4cc340d54c4004
Rakib <rakibpro98@gmail.com>
2023-10-05 12:41:10 +0600

    product delete api added

--- index.js
+++ index.js
@@ -1,12 +1,56 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+
+const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gaiflbq.mongodb.net/?retryWrites=true&w=majority`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+});
+
+const run = async () => {
+    try {
+        // Collection
+        const productsCollection = client.db('rkStore').collection('products');
+
+        // Products API
+        app.get('/inventory', async (req, res) => {
+            const query = {};
+            const cursor = productsCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        });
+        // Single Product API
+        app.get('/inventory/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const product = await productsCollection.findOne(query);
+            res.send(product);
+        });
+
+        // Delete Product API
+        app.delete('/inventory/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const result = await productsCollection.deleteOne(filter);
+            res.send(result);
+        });
+    } finally {
+    }
+};
+run().catch(console.dir);
 app.get('/', (req, res) => {
     res.send('This is RK-Store Server');
 });
