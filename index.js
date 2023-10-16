65690ebc0df4544428d1a30f8b3731c334b6b12b
Rakib <rakibpro98@gmail.com>
2023-10-16 13:14:26 +0600

    jwt token verification added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
@@ -9,6 +10,28 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
+
+// function to verify Token
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res
+            .status(401)
+            .send({ error: true, message: 'Unauthorized Access' });
+    }
+
+    const token = authorization.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res
+                .status(403)
+                .send({ error: true, message: 'forbidden access' });
+        }
+        req.decoded = decoded;
+        next();
+    });
+};
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gaiflbq.mongodb.net/?retryWrites=true&w=majority`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -24,6 +47,15 @@ const run = async () => {
     try {
         // Collection
         const productsCollection = client.db('rkStore').collection('products');
+
+        // JWT Token
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1h',
+            });
+            res.send({ token });
+        });
 
         // Products API
         app.get('/inventory', async (req, res) => {
@@ -42,14 +74,14 @@ const run = async () => {
         });
 
         // Add Product API
-        app.post('/inventory', async (req, res) => {
+        app.post('/inventory', verifyJWT, async (req, res) => {
             const product = req.body;
             const result = await productsCollection.insertOne(product);
             res.send(result);
         });
 
         // Update Product API
-        app.put('/inventory/:id', async (req, res) => {
+        app.put('/inventory/:id', verifyJWT, async (req, res) => {
             const id = req.params.id;
             const updatedProductQuantity = req.body.productQuantity;
             const updatedProductSold = req.body.productSold;
@@ -71,7 +103,7 @@ const run = async () => {
         });
 
         // Delete Product API
-        app.delete('/inventory/:id', async (req, res) => {
+        app.delete('/inventory/:id', verifyJWT, async (req, res) => {
             const id = req.params.id;
             const filter = { _id: new ObjectId(id) };
             const result = await productsCollection.deleteOne(filter);
